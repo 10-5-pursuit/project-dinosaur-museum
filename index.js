e7dfaae0a3fed59319385e485178b217d23b1145
eivor9 <njeremiah@pursuit.org>
2024-01-29 14:42:32 -0500

    Complete function definitions for dinosaurs.js and rooms.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const exampleDinosaurData = require("./data/dinosaurs");
+const exampleRoomData = require("./data/rooms");
+const exampleTicketData = require("./data/tickets");
+
 /// Program your functions below //
+
+function getConnectedRoomNamesById(rooms, id) {
+    let connectedRooms = [];
+  
+    for (const room of rooms)
+      if (room.roomId == id)
+        connectedRooms = room.connectsTo;
+  
+    const smallerRooms = rooms.reduce((group, x) => {
+      group[x.roomId] = x.name;
+      return group;
+    },{});
+  
+    return connectedRooms.map(x => smallerRooms[x]);
+  }
+
+console.log(getConnectedRoomNamesById(exampleRoomData, "zwfsfPU5u"));
