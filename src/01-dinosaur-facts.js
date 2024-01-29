f845c4e9a5a51a36a76a6d4e1afac14c1121f1e5
CSAN7690 <christophersanchez@pursuit.org>
2024-01-29 11:44:23 -0500

    First commit + updated with npm install

--- src/01-dinosaur-facts.js
+++ src/01-dinosaur-facts.js
@@ -22,7 +22,11 @@ const exampleDinosaurData = require("../data/dinosaurs");
  *  getLongestDinosaur(dinosaurs);
  *  //> { Brachiosaurus: 98.43 }
  */
-function getLongestDinosaur(dinosaurs) {}
+function getLongestDinosaur(dinosaurs) {
+  dinosaurs * 3.281;
+  return getLongestDinosaur
+}
+console.log(getLongestDinosaur);
 
 /**
  * getDinosaurDescription()
@@ -44,7 +48,7 @@ function getLongestDinosaur(dinosaurs) {}
  *  getDinosaurDescription(dinosaurs, "incorrect-id");
  *  //> "A dinosaur with an ID of 'incorrect-id' cannot be found."
  */
-function getDinosaurDescription(dinosaurs, id) {}
+function getDinosaurDescription(dinosaurs, id) { }
 
 /**
  * getDinosaursAliveMya()
@@ -71,7 +75,7 @@ function getDinosaurDescription(dinosaurs, id) {}
  *  getDinosaursAliveMya(dinosaurs, 65, "unknown-key");
  *  //> ["WHQcpcOj0G"]
  */
-function getDinosaursAliveMya(dinosaurs, mya, key) {}
+function getDinosaursAliveMya(dinosaurs, mya, key) { }
 
 module.exports = {
   getLongestDinosaur,
