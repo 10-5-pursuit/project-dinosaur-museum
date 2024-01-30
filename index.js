63d2363add95befb8a7b323092c58551f1e20182
llamouth <150949923+llamouth@users.noreply.github.com>
2024-01-30 08:01:53 -0500

    Complete ticket calculator problem 1

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 const exampleDinosaurData = require("./data/dinosaurs");
 const exampleRoomData = require("./data/rooms");
+const exampleTicketData = require("./data/tickets");
 /// Program your functions below //
-function getConnectedRoomNamesById(rooms, id) {
-    let roomArray = rooms.filter(room => room.connectsTo.includes(id));
-    if(roomArray.length == 0){
-        return `Room with ID of '${id}' could not be found.`
+function calculateTicketPrice(ticketData, ticketInfo) {
+    if(!ticketData.hasOwnProperty(ticketInfo.ticketType)){
+        return `Ticket type '${ticketInfo.ticketType}' cannot be found.`
     }
+    if(!ticketData.general.priceInCents.hasOwnProperty(ticketInfo.entrantType)){
+        return `Entrant type '${ticketInfo.entrantType}' cannot be found.`
+    }
+    if(!ticketData.membership.priceInCents.hasOwnProperty(ticketInfo.entrantType)){
+        return `Entrant type '${ticketInfo.entrantType}' cannot be found.`
+    }
+    if(!ticketData.extras.hasOwnProperty(ticketInfo.extras)){
+        return `Extra type '${ticketInfo.extras}' cannot be found.`
+    }
+    
 
-    let roomNameArray = roomArray.filter(room => room.name)
-    if(roomNameArray.length === 0){
-        return `Room with ID of '${id}' could not be found.`
+    let total = 0;
+    let entrant = ticketInfo.entrantType
+    if(ticketInfo.ticketType === "general"){
+        if(ticketData.general.priceInCents[entrant]){
+            total += ticketData.general.priceInCents[entrant]
+        }
+    }else {
+        if(ticketData.membership.priceInCents[entrant]){
+            total += ticketData.membership.priceInCents[entrant]
+        }
     }
-    return roomNameArray
+    if(ticketInfo.extras.length == 0){
+        return total
+    }else{
+        for(const x of ticketInfo.extras){
+            if(ticketData.extras.hasOwnProperty(x)){
+                total += ticketData.extras[x].priceInCents[entrant]
+            }
+        }
+    }
+    return total
 }
 
-console.log(getConnectedRoomNamesById(exampleRoomData, "Gp6nCN1JGT"))
+const ticketInfo = {
+    ticketType: "general",
+    entrantType: "child",
+    extras: ["movie"],
+};
+console.log(calculateTicketPrice(exampleTicketData, ticketInfo))
--- src/03-ticket-calculator.js
+++ src/03-ticket-calculator.js
@@ -54,7 +54,40 @@ const exampleTicketData = require("../data/tickets");
     calculateTicketPrice(tickets, ticketInfo);
     //> "Entrant type 'kid' cannot be found."
  */
-function calculateTicketPrice(ticketData, ticketInfo) {}
+function calculateTicketPrice(ticketData, ticketInfo) {
+  
+  if(!ticketData.hasOwnProperty(ticketInfo.ticketType)){
+    return `Ticket type '${ticketInfo.ticketType}' cannot be found.`
+  }
+
+  let total = 0;
+  let entrant = ticketInfo.entrantType
+
+  if(ticketInfo.ticketType === "general"){
+    if(!ticketData.general.priceInCents.hasOwnProperty(ticketInfo.entrantType)){
+      return `Entrant type '${ticketInfo.entrantType}' cannot be found.`
+    }
+    total += ticketData.general.priceInCents[entrant]
+  }else {
+    if(ticketData.membership.priceInCents[entrant]){
+      total += ticketData.membership.priceInCents[entrant]
+    }
+  }
+
+  if(ticketInfo.extras.length == 0){
+    return total
+  }else{
+    for(const x of ticketInfo.extras){
+      if(ticketData.extras.hasOwnProperty(x)){
+        total += ticketData.extras[x].priceInCents[entrant]
+      }else{
+        return `Extra type '${ticketInfo.extras}' cannot be found.`
+      }
+    }
+  }
+
+  return total
+}
 
 /**
  * purchaseTickets()
