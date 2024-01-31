e62a1aa05fe38addeb2633fe994fcbc8966cb2a0
Michael <mminier@pursuit.org>
2024-01-31 11:33:06 -0500

    created the function getDinosaurDescription for 01-dinosaur-facts

--- src/01-dinosaur-facts.js
+++ src/01-dinosaur-facts.js
@@ -38,7 +38,7 @@ function getLongestDinosaur(dinosaurs) {
   lengthInFeet = longestDinosaurLength * 3.281;
   const longestDinosaur = {};
     longestDinosaur [longestDinosaurName] = lengthInFeet
-
+ console.log(longestDinosaur)
 // Return the longest dinosaur and its height in feet.
   return longestDinosaur
 }
@@ -65,11 +65,24 @@ function getLongestDinosaur(dinosaurs) {
 
 const { dinosaurID, name, pronunciation, meaningOfName, diet, lengthInMeters, period, mya, info } = dinosaurs
 
+function getDinosaurDescription(dinosaurs, id) {
+  for (let key of dinosaurs){
+    let pronunciation = key.pronunciation
+    let name = key.name
+    let info = key.info
+    let period = key.period
+    let mya= key.mya
+  if(key.dinosaurId === id){
+    return `${name} (${pronunciation})\n${info} It lived in the ${period} period, over ${mya.length === 1 ? mya[0] : mya[1]} million years ago.`
+  }
+  
+  }
+  return `A dinosaur with an ID of '${id}' cannot be found.`
 
+}
+console.log(getDinosaurDescription(exampleDinosaurData, 'V53DvdhV2A'))
+    
 
-
-
-function getDinosaurDescription(dinosaurs, id) {}
 
 /**
  * getDinosaursAliveMya()
