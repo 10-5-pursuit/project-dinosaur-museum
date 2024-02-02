db798a75f4a221f9c664fcee246199f8c52810cb
llamouth <150949923+llamouth@users.noreply.github.com>
2024-02-02 02:12:42 -0500

    Refactor code and add comments

--- src/03-ticket-calculator.js
+++ src/03-ticket-calculator.js
@@ -169,87 +169,31 @@ function apply10Discount(price) {
   // This function takes in the price and applies a 10% discount to the price
   return price - (price / 10);
 }    
+
 function purchaseTickets(ticketData, purchases) {
   // Creating variables for the receipt and receiptDescription to assign values to  the variables
   let receipt = `Thank you for visiting the Dinosaur Museum!\n-------------------------------------------\n`;
   let receiptDescription = "";
   let total = 0;
-  
-  // ! Sinlge Purchase Ticket 
-  if(purchases.length === 1){
-    // Checking if the ticketdata has a key of the purchases ticket type
-    if(!ticketData.hasOwnProperty(purchases[0].ticketType)) {
-      // If it doesnt not exist within the ticketdata object return the given error message
-      return `Ticket type '${purchases[0].ticketType}' cannot be found.`
-    }
-    // Checking if the ticketdata tickettype princeInCenst has a key of the purchases entrant type
-    if(!ticketData[purchases[0].ticketType].priceInCents.hasOwnProperty(purchases[0].entrantType)){
-      // If it doesnt not exist within the ticketdata object return the given error message
-      return `Entrant type 'incorrect-entrant' cannot be found.`
-    }
-    // Creating variable and assinging them values of the purchases 
-    let price = ticketData[purchases[0].ticketType].priceInCents[purchases[0].entrantType]
-    let description = `${ticketData[purchases[0].ticketType].description}`;
-    let entrant = purchases[0].entrantType.slice(0,1).toUpperCase() + purchases[0].entrantType.slice(1).toLowerCase()
-    let extrasArr = purchases[0].extras;
-    // Checking if the extrasArray is Empty.
-    if(extrasArr.length === 0) {
-      // If The extrasArray is empty we just add the description to the receiptDescription and add the price to the total 
-      receiptDescription += `${entrant} ${description}: ${moneyConverter(String(price))}`
-      total += price;
-      // Apply a 10% discount if discount = true
-      if(purchases[0].discount === true){
-        price = apply10Discount(price)
-      }
-    // Else The extras array has elements in it so we need to process the extras array and get the descriptions and add the extras price to the total
-    }else {
-      // Checking if the extras object has the key of the given purchases extras
-      if(!ticketData.extras.hasOwnProperty(purchases[0].extras[0])){
-        // If it doesnt exist return the given error message
-        return `Extra type 'incorrect-extra' cannot be found.`
-      }
-      // Use reduce to create a string sentence of the extras descriptions
-      let extraStr = extrasArr.reduce((str, item, _, arr) => {
-        // If the current element is equal to the last item in the extras array
-        if(item === arr[arr.length - 1]){
-          // We add on to the string without a space at the end and add to the price of the current ticket 
-          str += `${ticketData.extras[item].description}`
-          price += ticketData.extras[item].priceInCents[purchases[0].entrantType]
-          return str
-        }else {
-          // Else if it is not the last item in the extras array we add to the string with a space and comma, then add to the price of the current ticket
-          str += `${ticketData.extras[item].description}, `
-          price += ticketData.extras[item].priceInCents[purchases[0].entrantType]
-          return str
-        }
-      },"")
-      // * Apply a discount of 10% if discount is true
-      if(purchases[0].discount === true){
-        price = apply10Discount(price)
-      }
-      // After the condition statements we add the price to the total and add to the receiptDescription the description of the current purchase
-      total += price;
-      receiptDescription += `${entrant} ${description}: ${moneyConverter(String(price))} (${extraStr})`
-    }
-    // return the receipt with the description and the total
-    return `${receipt}${receiptDescription}\n-------------------------------------------\nTOTAL: ${moneyConverter(String(total))}`
-  }
 
-  // ! Multiple Purchases 
-  // Else if it is not a single purchase it has multiple purchases so we have to loop. We then do the same ticketType and entryType check. 
+  // We loop through the purchases array. 
   for(let i = 0; i < purchases.length; i++) {
+    // Check if the purchases tickettype does not exist in the ticketdata array
     if(!ticketData.hasOwnProperty(purchases[i].ticketType)) {
+      // if true return given error message
       return `Ticket type '${purchases[i].ticketType}' cannot be found.`
     }
+    // Check if the purchases entranttype does not exist in the ticketdata array
     if(!ticketData[purchases[i].ticketType].priceInCents.hasOwnProperty(purchases[i].entrantType)){
-      return `Entrant type '${purchases[i].ticketType}' cannot be found.`
+      // If true return given error message
+      return `Entrant type '${purchases[i].entrantType}' cannot be found.`
     }
-    // Creating variable and assinging them values from the purchases
+    // Creating variables and assinging them values from the purchases
     let price = ticketData[purchases[i].ticketType].priceInCents[purchases[i].entrantType]
     let entrant = purchases[i].entrantType.slice(0,1).toUpperCase() + purchases[i].entrantType.slice(1).toLowerCase()
     let description = `${ticketData[purchases[i].ticketType].description}`;
     let extrasArr = purchases[i].extras
-    let extrasStrOfArrays = []
+    let extrasStr = []
     //  Checking if extrasArray is empty or not. If empty process the description and add to the total
     if(extrasArr.length === 0) {
       // Checking if the current index is on the last element in the extrasArr.\
@@ -271,8 +215,8 @@ function purchaseTickets(ticketData, purchases) {
           // If it doesnt exist return the given error message
           return `Extra type 'incorrect-extra' cannot be found.`
         }
-        // Check if the current element in the extrasArr is on the last element or if the extras arr only has one element we want to push the description without a space. If false push the description with a space. and then add the price 
-        j === extrasArr.length - 1 || extrasArr.length === 1 ? extrasStrOfArrays.push(`${ticketData.extras[extrasArr[j]].description}`) : extrasStrOfArrays.push(`${ticketData.extras[extrasArr[j]].description}, `)
+        // Check if the current element in the extrasArr is on the last element or if the extras arr only has one element we want the description without a space added to the extrasStr. If false add the description with a space to the extrasStr. and then add the price 
+        j === extrasArr.length - 1 || extrasArr.length === 1 ? extrasStr += `${ticketData.extras[extrasArr[j]].description}` : extrasStr +=`${ticketData.extras[extrasArr[j]].description}, `
         price += ticketData.extras[extrasArr[j]].priceInCents[purchases[i].entrantType]
       }
       // * If discount is true apply a 10% discount.
@@ -281,18 +225,13 @@ function purchaseTickets(ticketData, purchases) {
       }
       // Add the price to the total
       total += price
-      // Now we want to reduce the created array with string sentences to an a string with the description.
-      let extraStr = extrasStrOfArrays.reduce((str, item) => {
-        str += `${item}`
-        return str   
-      },"")
       // If the current index is on the last index we want to add on to the receiptDescription without a new line and break out the code
       if(i === purchases.length - 1){
-        receiptDescription += `${entrant} ${description}: ${moneyConverter(String(price))} (${extraStr})`
+        receiptDescription += `${entrant} ${description}: ${moneyConverter(String(price))} (${extrasStr})`
         break;
       }
       // else we add to the receiptDescription with a new line
-      receiptDescription += `${entrant} ${description}: ${moneyConverter(String(price))} (${extraStr})\n`
+      receiptDescription += `${entrant} ${description}: ${moneyConverter(String(price))} (${extrasStr})\n`
     }
   }
   // Return Finihsed receipt 
