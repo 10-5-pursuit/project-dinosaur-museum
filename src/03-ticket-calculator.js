752885cd1107168c159ffc211badcc21b7982b95
kbodur <kbodur@pursuit.org>
2024-02-02 19:27:23 -0500

    errorCheckforPurchase() added helper function to purchaseTickets()

--- src/03-ticket-calculator.js
+++ src/03-ticket-calculator.js
@@ -135,12 +135,44 @@ function calculateTicketPrice(ticketData, ticketInfo) {
     purchaseTickets(tickets, purchases);
     //> "Ticket type 'discount' cannot be found."
  */
-function purchaseTickets(ticketData, purchases) {
 
-
-
-  
+/**This is my helper funciton which checks if the purchase has valid Ticket type, Entrant type and extras. 
+** I used some and every native array functions to check if any value is in the purchase other than the valid extras which is eithe "education", "movie"
+** and "terrace" 
+** @param {Object} ticketData - An object containing data about prices to enter the museum. See the `data/tickets.js` file for an example of the input.
+** @param {Object[]} purchases - An array of objects. Each object represents a single ticket being purchased.
+** @returns {string} - a string explaining what is wrong.
+**/
+function errorCheckforPurchase(ticketData, purchases){
+  const extrlist = ['movie', 'terrace', 'education'];
+  for (let idx=0; idx<purchases.length; idx++){
+    if (!ticketData[purchases[idx].ticketType]) {
+      return `Ticket type '${purchases[idx].ticketType}' cannot be found.`;
+    }
+    if(purchases[idx].extras.length>0){
+      if (purchases[idx].extras.every(v => extrlist.includes(v))==false) {
+        return `Extra type '${purchases[idx].extras[0]}' cannot be found.`;
+      }
+    }
+    
+  if (!ticketData[purchases[idx].entrantType]) {
+    return `Entrant type '${purchases[idx].entrantType}' cannot be found.`;
+  }
 }
+
+}
+function purchaseTickets(ticketData, purchases) {
+  let receipt="Thank you for visiting the Dinosaur Museum!\n-------------------------------------------\n"
+  let total=0;
+  let str= errorCheckforPurchase(ticketData, purchases)
+   if (str.match("cannot be found.")){
+    return str;
+   }
+  
+
+}
+
+
 
 // Do not change anything below this line.
 module.exports = {
