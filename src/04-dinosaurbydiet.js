7daae236982d39cf4dc1a980fdf6f491494a6a65
kbodur <kbodur@pursuit.org>
2024-02-04 21:00:03 -0500

    new function for dinosaurs by diet explanation added

--- src/04-dinosaurbydiet.js
+++ src/04-dinosaurbydiet.js
@@ -16,18 +16,23 @@ const exampleDinosaurData = require("../data/dinosaurs");
  */
 
 function getDinosaursByDiet(dinosaurs){
+ // Three arrays (carnivorous, herbivorous, omnivorous) are initialized to store dinosaur names based on their diets. The diets object will be used to store these arrays.   
 let carnivorous=[],herbivorous=[],omnivorous=[]
 let diets={}
+// The function uses forEach to iterate through each dinosaur in the dinosaurs array.
 dinosaurs.forEach(din=>{
     if(din.diet=='carnivorous') carnivorous.push(din.name)
     if(din.diet=='herbivorous') herbivorous.push(din.name)
     if(din.diet=='omnivorous') omnivorous.push(din.name)
 })
+// Inside the loop, the function checks each dinosaur's diet and adds its name to the corresponding array (carnivorous, herbivorous, or omnivorous).
+
 diets["carnivorous"]=carnivorous
 diets["herbivorous"]=herbivorous
 diets["omnivorous"]=omnivorous
-
-return diets
+// After iterating through all dinosaurs, the function creates the diets object with keys representing diet types and values as arrays containing the names of dinosaurs with those diets.
+return diets;
+// The function returns the final object, where dinosaurs are categorized by their diets.
 }
 
 module.exports = {
